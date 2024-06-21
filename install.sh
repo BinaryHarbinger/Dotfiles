10bdeef1c422d1ae666e75594778016cf2b21342
Halil Efe ESEN <132145397+BinaryHarbinger@users.noreply.github.com>
2024-06-21 12:23:46 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-yay -S ttf-fira-code otf-fira-code-symbol hyprlock hyprpaper wlogout kitty
+yay -S ttf-fira-code otf-fira-code-symbol hyprlock hyprpaper wlogout kitty fuzzel
 cp -r ./waybar ~/.config/
 cp -r ./hypr ~/.config/
 cp -r ./kitty ~/.config/
