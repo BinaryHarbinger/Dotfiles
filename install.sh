96f9a359f2e928a7d7a38dc09bf627a70ce5c767
Halil Efe ESEN <132145397+BinaryHarbinger@users.noreply.github.com>
2024-07-23 20:04:07 +0300

    Update install.sh
    Added a line to fix swaync functions

--- install.sh
+++ install.sh
@@ -3,8 +3,11 @@ git clone https://github.com/lbonn/rofi.git
 cd rofi
 meson build
 
+sed -i "s/USERNAME/${USER}/g" ./swaync/config.json
+
 cp -r ./waybar ~/.config/
 cp -r ./hypr ~/.config/
 cp -r ./kitty ~/.config/
 cp -r ./rofi ~/.config/
 cp -r ./wlogout ~/.config/
+cpr -r ./swaync ~/.config/
