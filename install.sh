da2d1fec03724ebfa75b1352f2ae10103270c021
Halil Efe ESEN <132145397+BinaryHarbinger@users.noreply.github.com>
2024-11-24 12:15:59 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
-yay -Syu --noconfirm ttf-fira-code otf-fira-code-symbol hyprland yazi micro rofi-wayland hyprlock hyprpaper wlogout kitty alacritty papirus-icon-theme base-devel git waybar swaync network-manager-applet
+yay -Syu --noconfirm git ttf-fira-code otf-fira-code-symbol hyprland yazi micro rofi-wayland hyprlock hyprpaper wlogout kitty alacritty papirus-icon-theme base-devel git waybar swaync network-manager-applet
+
+git clone https://github.com/BinaryHarbinger/dotfiles.git
+cd dotfiles
+
+rm -r preview readme.md
 
 sed -i "s/USERNAME/${USER}/g" ./swaync/config.json
 
-cp -r ./* ~/.config/
+mv -r ./* ~/.config/
