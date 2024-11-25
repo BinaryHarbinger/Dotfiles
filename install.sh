0fe4dee7b807bd36137d52442429b6f509e0e598
Halil Efe ESEN <132145397+BinaryHarbinger@users.noreply.github.com>
2024-11-25 20:12:12 +0300

    Spicetify installation and auto configuration applied

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-yay -Syu --noconfirm git nwg-look qt6ct fish power-profiles-daemon fastfetch ttf-fira-code otf-fira-code-symbol hyprland yazi micro rofi-wayland hyprlock hyprpaper wlogout kitty alacritty papirus-icon-theme base-devel git waybar swaync network-manager-applet
+yay -Syu --noconfirm spicetify-cli git nwg-look qt6ct fish power-profiles-daemon fastfetch ttf-fira-code otf-fira-code-symbol hyprland yazi micro rofi-wayland hyprlock hyprpaper wlogout kitty alacritty papirus-icon-theme base-devel git waybar swaync network-manager-applet
+
+curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.sh | sh
 
 git clone https://github.com/BinaryHarbinger/dotfiles.git
 cd dotfiles
@@ -10,5 +12,7 @@ mv ./.scripts ~
 chmod +x ~/.scripts/*
 mv ./* ~/.config/
 chmod +x ~/.config/hypr/scripts/*
+
+spicetify apply
 
 chsh -s /bin/fish
