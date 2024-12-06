6cc5431f989d276eccdd6668f45aca8d9365385a
Halil Efe ESEN <132145397+BinaryHarbinger@users.noreply.github.com>
2024-12-06 14:32:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Binary Harbingers Dotfiles v1.7"
+echo "Binary Harbingers Dotfiles v1.8.5"
 
 # First, update the system, but skip already installed packages
 echo "Updating system..."
@@ -9,13 +9,16 @@ yay -Syu --noconfirm --needed
 # Install required packages, but skip already installed ones
 PACKAGES=(
   breeze cliphist spicetify-cli-git git nwg-look qt6ct fish power-profiles-daemon fastfetch
-  ttf-fira-code otf-fira-code-symbol hyprland yazi micro rofi-wayland hyprlock
-  hyprpaper wlogout kitty alacritty papirus-icon-theme base-devel waybar swaync
+  ttf-fira-code otf-fira-code-symbol hyprland yazi micro rofi-wayland hyprlock hyprpolkitagent-git
+  hyprpaper wlogout kitty alacritty papirus-icon-theme base-devel waybar swaync mpv
   network-manager-applet materia-gtk-theme colloid-icon-theme-git pamixer brightnessctl hyprswitch avizo
 )
 
 echo "Installing packages..."
 yay -S --noconfirm --needed "${PACKAGES[@]}"
+
+echo "Setting up polkit agent"
+systemctl --user enable --now hyprpolkitagent.service
 
 # Install Spicetify
 echo "Installing Spicetify..."
