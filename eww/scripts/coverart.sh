9db281d4bc91c95efb80326843ac9419e53f9627
Halil Efe ESEN <132145397+BinaryHarbinger@users.noreply.github.com>
2025-01-01 01:13:18 +0300

    More eww optimizations.

--- eww/scripts/coverart.sh
+++ eww/scripts/coverart.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
+# Config dosya yolları
+CONFIG_DIR="$HOME/.config/eww"
+COVER_ART_FILE="$CONFIG_DIR/coverart.png"
+INFO_FILE="$CONFIG_DIR/info.txt"
+
+# Çalınan parçanın bilgilerini al
+current_title=$(playerctl metadata --format "{{ title }}")
 url=$(playerctl metadata --format "{{ mpris:artUrl }}" | sed 's/b273/1e02/')
 
-if [[ "$url" == https* ]]; then
-    curl -s -o coverart.png "$url"
-    echo "file://$HOME/.config/eww/coverart.png"
+# Eski şarkı bilgisini oku
+if [[ -f "$INFO_FILE" ]]; then
+    last_title=$(cat "$INFO_FILE")
 else
-    echo "$url"
+    last_title=""
+fi
+
+# Eğer çalınan parça değişmişse veya info.txt yoksa
+if [[ "$current_title" != "$last_title" ]]; then
+    echo "$current_title" > "$INFO_FILE" # Yeni şarkı adını kaydet
+
+    # URL geçerliyse resmi indir
+    if [[ "$url" == https* ]]; then
+        curl -s -o "$COVER_ART_FILE" "$url"
+        echo "file://$COVER_ART_FILE"
+    else
+        echo "$url"
+    fi
+else
+    # Şarkı değişmediyse mevcut resim dosyasını döndür
+    echo "file://$COVER_ART_FILE"
 fi
