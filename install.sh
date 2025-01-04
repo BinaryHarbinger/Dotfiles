845f6d543d8e75f44506cb184c95838c732ddf72
Halil Efe ESEN <132145397+BinaryHarbinger@users.noreply.github.com>
2025-01-04 16:14:27 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ PACKAGES=(
   breeze cliphist git nwg-look qt6ct fish power-profiles-daemon fastfetch ttf-jetbrains-mono-nerd ttf-jetbrains-mono
   ttf-fira-code otf-fira-code-symbol hyprland yazi micro rofi-wayland hyprlock hyprpolkitagent unzip hyprsunset rofimoji
   hyprpaper wlogout kitty alacritty papirus-icon-theme base-devel waybar swaync mpv hyprpicker eww pamixer hypridle
-  network-manager-applet pamixer brightnessctl hyprswitch avizo bibata-cursor-theme catppuccin-gtk-theme-mocha
+  network-manager-applet pamixer brightnessctl hyprswitch avizo bibata-cursor-theme catppuccin-gtk-theme-mocha ttf-material-design-iconic-font
 )
 
 echo "Installing packages..."
