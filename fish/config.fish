84ccc7d260e81dd20ab1d6f0d221abd812e060cc
BinaryHarbinger <132145397+BinaryHarbinger@users.noreply.github.com>
2025-04-29 21:03:18 +0300

    Fixed user scripts path
    * User scripts path was defined by my user name. Changed with ~/.scripts for compability for another users.

--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -Ux fish_user_paths $fish_user_paths /home/efe/.scripts
-neofetch
+set -Ux fish_user_paths $fish_user_paths ~/.scripts
+fastfetch
 
 # Created by `pipx` on 2025-02-20 15:01:21
-set PATH $PATH /home/efe/.local/bin
+set PATH $PATH ~/.local/bin
